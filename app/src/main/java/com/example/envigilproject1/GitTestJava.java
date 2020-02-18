81f739ff54c06b7d0d9f068cf5886b97232c66b9
Izhar <izhark666@gmail.com>
2020-02-18 15:11:20 +0530

    Test 2 of the git repo

--- app/src/main/java/com/example/envigilproject1/GitTestJava.java
+++ app/src/main/java/com/example/envigilproject1/GitTestJava.java
@@ -4,4 +4,8 @@ public class GitTestJava {
     public static void main(String[] args) {
         System.out.println("This is testing git repo");
     }
+
+    public static void main1(String[] args) {
+        System.out.println("This is test 2 git repo");
+    }
 }
